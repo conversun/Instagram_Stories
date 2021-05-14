394a68516a2c537acf785136babef101ae786b46
xuteng <m18374845793@163.com>
2021-05-14 15:27:49 +0800

    1. share btn hidden

--- InstagramStories/Stories/StoryHeaderView & ProgressView/IGStoryPreviewBottomView.swift
+++ InstagramStories/Stories/StoryHeaderView & ProgressView/IGStoryPreviewBottomView.swift
@@ -21,12 +21,12 @@ class IGStoryPreviewBottomView: UIView {
     var needPauseBlock: ((Bool) -> Void)?
     
     let download = UIButton()
-        .image("ic_download_userprofile")
+        .image("ic_download_story")
 //    let downloadVip = UIImageView()
 //        .image("ic_pro_color")
 //        .isUserInteractionEnabled(false)
-    let share = UIButton()
-        .image("ic_repost_userprofile")
+//    let share = UIButton()
+//        .image("ic_repost_userprofile")
     let bgView = UIView()
     
     public var snap: IGSnap? {
@@ -44,66 +44,66 @@ class IGStoryPreviewBottomView: UIView {
 //            .drive(downloadVip.rx.isHidden)
 //            .disposed(by: disposeBag)
         
-        share.rx
-            .tap
-            .bind(onNext: { [weak self] in
-                guard let `self` = self else { return }
-                guard let url = self.snap?.url else { return }
-                
-                switch self.snap?.kind {
-                case .image:
-                    guard let url = url.url else { return }
-                    HUD.show()
-                    self.needPauseBlock?(true)
-                    KingfisherManager.shared.retrieveImage(with: ImageResource(downloadURL: url)) { result in
-                        HUD.hide()
-                        switch result {
-                        case let .success(response):
-                            let visibleVC = UIApplication.rootController?.visibleVC
-                            let vc = UIActivityViewController(activityItems: [response.image], applicationActivities: nil)
-                            vc.excludedActivityTypes = [.saveToCameraRoll]
-                            if let popOver = vc.popoverPresentationController {
-                                popOver.sourceView = visibleVC?.view
-                                popOver.sourceRect = visibleVC?.view.convert(self.share.frame, from: self.share.superview ?? self.share) ?? self.share.bounds
-                            }
-                            vc.completionWithItemsHandler = { _, completed, items, error in
-                                self.needPauseBlock?(false)
-                            }
-                            visibleVC?.present(vc, animated: true, completion: nil)
-                        case let .failure(error):
-                            self.needPauseBlock?(false)
-                            HUD.error(error.localizedDescription)
-                        }
-                    }
-                case .video:
-                    
-                    HUD.show()
-                    self.needPauseBlock?(true)
-                    IGVideoCacheManager.shared.getFile(for: url) { result in
-                        HUD.hide()
-                        switch result {
-                        case let .success(response):
-                            let visibleVC = UIApplication.rootController?.visibleVC
-                            let vc = UIActivityViewController(activityItems: [response], applicationActivities: nil)
-                            vc.excludedActivityTypes = [.saveToCameraRoll]
-                            if let popOver = vc.popoverPresentationController {
-                                popOver.sourceView = visibleVC?.view
-                                popOver.sourceRect = visibleVC?.view.convert(self.share.frame, from: self.share.superview ?? self.share) ?? self.share.bounds
-                            }
-                            vc.completionWithItemsHandler = { _, completed, items, error in
-                                self.needPauseBlock?(false)
-                            }
-                            UIApplication.rootController?.visibleVC?.present(vc, animated: true, completion: nil)
-                        case let .failure(error):
-                            self.needPauseBlock?(false)
-                            HUD.error(error.localizedDescription)
-                        }
-                    }
-                default:
-                    break
-                }
-            })
-            .disposed(by: disposeBag)
+//        share.rx
+//            .tap
+//            .bind(onNext: { [weak self] in
+//                guard let `self` = self else { return }
+//                guard let url = self.snap?.url else { return }
+//
+//                switch self.snap?.kind {
+//                case .image:
+//                    guard let url = url.url else { return }
+//                    HUD.show()
+//                    self.needPauseBlock?(true)
+//                    KingfisherManager.shared.retrieveImage(with: ImageResource(downloadURL: url)) { result in
+//                        HUD.hide()
+//                        switch result {
+//                        case let .success(response):
+//                            let visibleVC = UIApplication.rootController?.visibleVC
+//                            let vc = UIActivityViewController(activityItems: [response.image], applicationActivities: nil)
+//                            vc.excludedActivityTypes = [.saveToCameraRoll]
+//                            if let popOver = vc.popoverPresentationController {
+//                                popOver.sourceView = visibleVC?.view
+//                                popOver.sourceRect = visibleVC?.view.convert(self.share.frame, from: self.share.superview ?? self.share) ?? self.share.bounds
+//                            }
+//                            vc.completionWithItemsHandler = { _, completed, items, error in
+//                                self.needPauseBlock?(false)
+//                            }
+//                            visibleVC?.present(vc, animated: true, completion: nil)
+//                        case let .failure(error):
+//                            self.needPauseBlock?(false)
+//                            HUD.error(error.localizedDescription)
+//                        }
+//                    }
+//                case .video:
+//
+//                    HUD.show()
+//                    self.needPauseBlock?(true)
+//                    IGVideoCacheManager.shared.getFile(for: url) { result in
+//                        HUD.hide()
+//                        switch result {
+//                        case let .success(response):
+//                            let visibleVC = UIApplication.rootController?.visibleVC
+//                            let vc = UIActivityViewController(activityItems: [response], applicationActivities: nil)
+//                            vc.excludedActivityTypes = [.saveToCameraRoll]
+//                            if let popOver = vc.popoverPresentationController {
+//                                popOver.sourceView = visibleVC?.view
+//                                popOver.sourceRect = visibleVC?.view.convert(self.share.frame, from: self.share.superview ?? self.share) ?? self.share.bounds
+//                            }
+//                            vc.completionWithItemsHandler = { _, completed, items, error in
+//                                self.needPauseBlock?(false)
+//                            }
+//                            UIApplication.rootController?.visibleVC?.present(vc, animated: true, completion: nil)
+//                        case let .failure(error):
+//                            self.needPauseBlock?(false)
+//                            HUD.error(error.localizedDescription)
+//                        }
+//                    }
+//                default:
+//                    break
+//                }
+//            })
+//            .disposed(by: disposeBag)
         
         download.rx
             .tap
@@ -176,7 +176,8 @@ class IGStoryPreviewBottomView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        [bgView, download, share]
+//        [bgView, download, share]
+        [bgView, download]
             .forEach { $0.adhere(toSuperview: self) }
         observer()
         
@@ -187,8 +188,9 @@ class IGStoryPreviewBottomView: UIView {
         
         bgView.pin.left().right().bottom().height(44 + (UIApplication.rootController?.rootVC?.view.safeArea.bottom ?? 0))
         
-        share.pin.right(8).bottom(8 + (UIApplication.rootController?.rootVC?.view.safeArea.bottom ?? 0)).size(44)
-        download.pin.before(of: share, aligned: .center).size(44).marginRight(8)
+//        share.pin.right(8).bottom(8 + (UIApplication.rootController?.rootVC?.view.safeArea.bottom ?? 0)).size(44)
+        download.pin.right(8).bottom(8 + (UIApplication.rootController?.rootVC?.view.safeArea.bottom ?? 0)).size(44)
+//        download.pin.before(of: share, aligned: .center).size(44).marginRight(8)
 //        downloadVip.pin.sizeToFit().before(of: share, aligned: .top).marginRight(-6)
         
         bgView.gradientVertical(.clear, UIColor.black.withAlphaComponent(0.5))
