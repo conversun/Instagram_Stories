2ebb06f4fe79eb33abb1230d6626f4e6165532f4
xuteng <m18374845793@163.com>
2021-05-28 11:26:36 +0800

    1. fix bug 2. add array 去重

--- InstagramStories/Stories/Extension/IGAdditions.swift
+++ InstagramStories/Stories/Extension/IGAdditions.swift
@@ -32,4 +32,16 @@ extension Array {
             }
         })
     }
+    
+    // 去重
+    func filterDuplicates<E: Equatable>(_ filter: (Element) -> E) -> [Element] {
+        var result = [Element]()
+        for value in self {
+            let key = filter(value)
+            if !result.map({filter($0)}).contains(key) {
+                result.append(value)
+            }
+        }
+        return result
+    }
 }
